ebda1c9e0c8cf948ea7daa6adffc93dbfb1e2547
Gercek Karakus <gercekk@gmail.com>
2014-04-19 23:41:24 -0700

    clones backbone boiler plate and adds bootstrap on top of it

---
+++ dist/app/config.js
@@ -0,0 +1,12 @@
+// This is the runtime configuration file.  It complements the Gruntfile.js by
+// supplementing shared properties.
+require.config({
+  paths: {
+    "vendor": "../vendor",
+    "almond": "../vendor/bower/almond/almond",
+    "underscore": "../vendor/bower/lodash/dist/lodash.underscore",
+    "jquery": "../vendor/bower/jquery/dist/jquery",
+    "backbone": "../vendor/bower/backbone/backbone",
+    "bootstrap": "../vendor/bower/bootstrap/dist/js/bootstrap"
+  }
+});
